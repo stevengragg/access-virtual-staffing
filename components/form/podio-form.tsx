9bcdcccba9abe94144a22077816966f0e888c093
Steven Gragg <sgragg5619@gmail.com>
2024-12-09 05:15:11 +0800

    Fix: Updated the hiring form with new podio webform

--- components/form/podio-form.tsx
+++ components/form/podio-form.tsx
@@ -7,10 +7,13 @@ export const PodioForm = (props: Props) => {
       id="podio_form_container"
       className="lg:px-[5%] py-16 md:py-24 lg:py-28"
     >
-      <div className="container-xl h-[1720px] overflow-hidden">
+      <div className="container-xl h-[1820px] overflow-hidden">
         <Suspense fallback={<p>Loading form...</p>}>
           <iframe
-            src="https://podio.com/webforms/29746582/2461117"
+            src={
+              process.env.NEXT_PUBLIC_PODIO_WEB_FORM ||
+              "https://podio.com/webforms/29979527/2496589"
+            }
             frameBorder="0"
             className="w-full h-full"
           />
