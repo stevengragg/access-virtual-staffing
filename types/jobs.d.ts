2b07f88ea3cec84724670713bbba4f4e7104b205
Kobe Brian Santos <kobebrian.santos.e@gmail.com>
2025-03-02 02:56:46 +0800

    feat: added job related interfaces

--- types/jobs.d.ts
+++ types/jobs.d.ts
@@ -5,3 +5,28 @@ export type JobSearchFilter = {
 export interface ISearchParams {
   [key: string]: string | string[] | undefined;
 }
+
+export interface IJobApplicationDetails {
+  id: number;
+  resume: string;
+  salaryMin: number;
+  salaryMax: number;
+  salaryCurrency: string;
+  location: string;
+  jobEquity: string;
+  about: string;
+  responsibilities: string;
+  requirements: string;
+  relocation: boolean;
+  experience: string;
+}
+
+export interface IJobApplication {
+  id: number;
+  company: string;
+  position: string;
+  status: "Pending" | "Not Accepted" | "Accepted";
+  date: string;
+  logo: string;
+  job?: IJob;
+}
