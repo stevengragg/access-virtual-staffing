be69add09ab4e203b74c6cf3e193d7d5e691f2a6
Kobe Brian Santos <kobebrian.santos.e@gmail.com>
2025-03-02 02:58:29 +0800

    feat: created base layouts

--- app/(applicant)/app/submissions/page.tsx
+++ app/(applicant)/app/submissions/page.tsx
@@ -1,19 +1,11 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import SubmissionPages from "@/components/submissions/tab-holder";
 
 export default withPageAuthRequired(
   async function Submissions() {
     return (
       <div className="h-[calc(100vh-4.5rem)] overflow-auto">
-        <div className="border-b-2 border-dashed border-[#d3d3d3] py-6 text-center text-black/50">
-          <h1>Submissions</h1>
-        </div>
-        <div className="container px-6 py-8 md:px-8 md:py-10 lg:py-12">
-          <div className="grid grid-cols-1 gap-12">
-            <div className="flex h-screen items-center justify-center border-2 border-dashed border-[#d3d3d3] py-6 text-center text-black/50">
-              <h2>Click and paste Main Content</h2>
-            </div>
-          </div>
-        </div>
+        <SubmissionPages />
       </div>
     );
   },
