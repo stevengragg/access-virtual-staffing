d95be7ccbae87e626d015a70499ba0f2c1ef2628
leerickduqueza <leerickduqueza@example.com>
2025-03-25 19:26:29 +0800

    feat: implement improvement on applicant portal page

--- app/(public)/talent/portal/page.tsx
+++ app/(public)/talent/portal/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import Image from "next/image";
 import { AccessPortalContainer } from "@/components/auth/access-portal-container";
 
 export const metadata: Metadata = {
@@ -22,23 +23,42 @@ export const metadata: Metadata = {
   },
 };
 
+const image = {
+  src: "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?auto=format&fit=crop&w=1920&q=80",
+  alt: "Modern office workspace",
+  width: 1920,
+  height: 1080,
+};
+
 export default function Auth() {
   return (
-    <section className="flex items-center justify-center min-h-screen px-6 bg-gray-50">
-      <div className="w-full max-w-lg bg-white p-10 rounded-3xl shadow-2xl border border-gray-200">
+    <section className="relative flex items-center justify-center min-h-screen">
+      <div className="absolute inset-0 z-0">
+        <Image
+          src={image.src}
+          alt={image.alt}
+          className="w-full h-full object-cover"
+          layout="fill"
+          priority
+        />
+        <div className="absolute inset-0 bg-deepBlue bg-opacity-70"></div>
+      </div>
+
+      {/* Centered Card */}
+      <div className="relative z-10 w-full max-w-md bg-white p-12 rounded-3xl shadow-[0px_10px_80px_rgba(0,0,0,0.7)] border border-gray-200 bg-opacity-90 backdrop-blur-lg">
         {/* Header Section */}
         <div className="text-center">
-          <h1 className="text-4xl font-bold text-gray-900">Applicant Portal</h1>
-          <p className="mt-3 text-lg text-gray-600">
+          <h1 className="text-3xl font-extrabold text-gray-900">
+            Applicant Portal
+          </h1>
+          <p className="mt-4 text-md text-gray-600 leading-relaxed">
             Find jobs that match your skills and access your account to manage
             applications.
           </p>
         </div>
 
-        {/* Login & Signup Buttons */}
-        <div className="mt-8">
-          <AccessPortalContainer />
-        </div>
+        {/* Login & Signup Button */}
+        <AccessPortalContainer />
       </div>
     </section>
   );
