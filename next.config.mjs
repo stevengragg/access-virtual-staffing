f3baaca72ca64058e9c6dd88ef0f54363e5865cf
Steven Gragg <sgragg5619@gmail.com>
2025-06-09 20:22:38 +0800

    feat: integrate Payload CMS with new configuration and types; add media and user collections, implement redirects, and enhance environment configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,22 @@
+import { withPayload } from "@payloadcms/next/withPayload";
+import redirects from "./redirects.mjs";
+
+const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : undefined || process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
+      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+        const url = new URL(item);
+
+        return {
+          hostname: url.hostname,
+          protocol: url.protocol.replace(":", ""),
+        };
+      }),
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
@@ -31,8 +46,14 @@ const nextConfig = {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
       },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
     ],
   },
+  reactStrictMode: true,
+  redirects,
 };
 
-export default nextConfig;
+export default withPayload(nextConfig, { devBundleServerPackages: false });
