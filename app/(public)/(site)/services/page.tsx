9e51037cdab545d3ea2e6c8f387ac6eac30ae50c
Steven Gragg <sgragg5619@gmail.com>
2025-06-24 21:29:49 +0800

    chore: temporary

--- app/(public)/(site)/services/page.tsx
+++ app/(public)/(site)/services/page.tsx
@@ -33,17 +33,18 @@ type Props = {};
 export default function Services({}: Props) {
   return (
     <main className="w-full mx-auto bg-neutralLightZinc overflow-hidden">
+      <h1>....</h1>
       {/* Header Section */}
-      <HeroHeaderNormal
+      {/* <HeroHeaderNormal
         heading="Our Services"
         context="At Access Virtual Staffing, we offer a range of tailored recruitment solutions to meet your business needs. Whether you're seeking general virtual assistance, specialized skills, or high-level professional expertise, our service tiers are designed to provide the right support. From Basic Plan to Specialized Services, we ensure you get the talent and tools necessary to drive your business forward."
         ads={{
           text: "For short-term or specialized projects, we also partner with platforms like Fiverr Pro, offering quick access to top-rated freelancers.",
           url: process.env.NEXT_PUBLIC_FIVERR_AFFILIATE_URL,
         }}
-      />
+      /> */}
       {/* Services A Section */}
-      <ServicesLeft
+      {/* <ServicesLeft
         heading="Basic Plan:  Recruiting and Payroll"
         description="Our Basic Recruitment and Payroll service is ideal for employers seeking project-based staff. We handle the recruiting and payroll processes, allowing you to focus on the project, without the need for timekeeping or benefits administration."
         button={{
@@ -63,9 +64,9 @@ export default function Services({}: Props) {
           width: 616,
           height: 640,
         }}
-      />
+      /> */}
       {/* Services B Section */}
-      <ServicesRight
+      {/* <ServicesRight
         heading="Standard Plan:  Advance Recruiting Solutions"
         description="Our Standard plan offers a comprehensive solution for your staffing needs, including recruiting, payroll, timekeeping, and benefits administration. Ideal for businesses seeking full-service support, this plan ensures seamless management of your workforce, allowing you to focus on your core operations."
         button={{
@@ -85,10 +86,10 @@ export default function Services({}: Props) {
           width: 616,
           height: 640,
         }}
-      />
+      /> */}
 
       {/* Services C Section */}
-      <ServicesLeft
+      {/* <ServicesLeft
         heading="Specialized Services"
         description="Our Specialized Services go beyond recruitment to offer tailored training programs that elevate your team’s capabilities. From Complete Staff Work and Basic Management training to Advanced Leadership training, we cover essential skills such as job description writing, goal setting, SWOT analysis, and process improvement to ensure your virtual staff excels and drives your business forward."
         button={{
@@ -108,10 +109,10 @@ export default function Services({}: Props) {
           width: 616,
           height: 640,
         }}
-      />
+      /> */}
 
       {/* Hero Feature Section */}
-      <HeroFeature />
+      {/* <HeroFeature /> */}
       {/* Testimonials Section */}
       {/* <Testimonials /> */}
       {/* CTA Footer Section */}
