82c06dd7dbc3ea61c5afb31df3dd7976052cbd02
Steven Gragg <sgragg5619@gmail.com>
2025-07-04 17:41:14 +0800

    refactor: Standardize URL handling and improve sitemap generation logic across routes; update default site URL and enhance code formatting for consistency.

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,16 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import redirects from "./redirects.mjs";
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  ? process.env.VERCEL_PROJECT_PRODUCTION_URL
   : undefined || process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+      ...[
+        NEXT_PUBLIC_SERVER_URL /* 'https://www.accessvirtualstaffing.com' */,
+      ].map((item) => {
         const url = new URL(item);
 
         return {
